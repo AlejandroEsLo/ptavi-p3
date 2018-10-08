7b95fa8676d434953b0788c39ae09c2980f9d7d3
AlejandroEsLo <alex-lojim@hotmail.com>
2018-10-08 19:36:49 +0200

    Falta terminar lista final

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -5,61 +5,42 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
 class SmallSMILHandler(ContentHandler):
-    """
-    Clase para manejar etiquetas SMIL
-    """
 
     def __init__ (self):
-        """
-        Constructor. Inicializamos las variables
-        """
-        self.etiqueta = ""
-        self.width = ""
-        self.height = ""
-        self.backgroundcolor = ""
-        self.id = ""
-        self.top = ""
-        self.bottom = ""
-        self.left = ""
-        self.right = ""
-        self.dicc = []
+
+        #Creamos listas con sus correspondientes atributos
+        self.rootlayout = ["width","height","background-color"]
+        self.region = ["id","top","bottom","left","right"]
+        self.img = ["src","region","begin","dur"]
+        self.audio = ["src","begin","dur"]
+        self.textstream = ["src","region"]
+        
+        #Lista vacia para ir almacenando las listas anteriores al rellenarse
+        self.data = []
     
     def startElement(self, etiqueta, attrs):
-              
-        if etiqueta == 'root-layout':
-            # De esta manera tomamos los valores de los atributos
-            self.etiqueta = etiqueta
-           
-            self.width = attrs.get('width', "")
-            self.height = attrs.get('height', "")
-            self.backgroundcolor = attrs.get('background-color', "")
-            self.data = self.width + self.height + self.backgroundcolor
-            self.dicc = (etiqueta,self.width,self.height,self.backgroundcolor)
-          
-            print(self.dicc)
-            
-        elif etiqueta == 'region':
-            
-            self.etiqueta = etiqueta
-            self.id = attrs.get('id', "")  
-            self.top = attrs.get('top', "")
-            self.left = attrs.get('left', "")
-            self.dicc = (self.etiqueta,self.id,self.top,self.left)
-            
-            print(self.dicc)
-            
+
+        #Creamos un diccionario con la etiqueta y su atributo correspondiente
+        self.dicc = {"root-layout": self.rootlayout, "region": self.region,
+                     "img": self.img, "audio": self.audio,
+                     "textstream": self.textstream}
+
+#        dicc_aux ={}  >> ir almacenando
+
+        if etiqueta in self.dicc:
+
+            self.data.append(etiqueta)# Añadir al final de la lista
+      
     def get_tags(self):
        
-       return self.dicc
+       return self.data
 
 if __name__ == "__main__":
-    """
-    Programa principal
-    """
+
     parser = make_parser()#Creo parser
     cHandler = SmallSMILHandler()#Creo manejador
     parser.setContentHandler(cHandler)#Le paso el parser al manejador
-    parser.parse(open('karaoke.smil'))#Leo lineas parseando
- #   print(cHandler.startElement(etiqueta,attrs))
+    parser.parse(open('karaoke.smil'))
+    print(cHandler.get_tags())
     
     
