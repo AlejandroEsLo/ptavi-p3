5597d1e7411221f2aeaa230e18e3747107eac0b2
AlejandroEsLo <alex-lojim@hotmail.com>
2018-10-13 12:26:59 +0200

    Descarga archivos hasta el hello.wav, despues da error..(solucionar)

--- karaoke.py
+++ karaoke.py
@@ -7,11 +7,10 @@ from xml.sax.handler import ContentHandler
 import sys
 import json
 
+from urllib.request import urlretrieve
 from smallsmilhandler import SmallSMILHandler
 
 try:
-    my_class = SmallSMILHandler()
-    
     fich_smil = sys.argv[1]
     
     fich_json = sys.argv[1].replace(".smil",".json")
@@ -20,25 +19,39 @@ try:
     my_class = SmallSMILHandler()
     parser.setContentHandler(my_class)
     parser.parse(open(fich_smil))
-    with open(fich_json, "w") as fichToJson:
-    
+     
+    with open(fich_json, "w") as fichToJson:    
     
         my_archv = my_class.get_tags()
-    
+        archv_smil = ""
         linea_final = ""
         for linea in my_archv:
             elementoX = linea[0]
             atributoXY = linea[1]
             
+            #Iteramos por el archivo            
             for valorXY in atributoXY:
                 if atributoXY[valorXY] != "":
                     elementoX += "\\t" + str(valorXY) + "=\"" + str(atributoXY[valorXY]) + "\""             
                 
-                linea_final = elementoX + "\\n"       
-    
+                linea_final = elementoX + "\\n"  
+                
+            archv_smil = archv_smil + linea_final + "\n"
+
+            #Fichero final Json            
             json.dump(linea_final,fichToJson)
-            
-            print(linea_final)
+
+            #Descargamos archivos url http://            
+            for valorXY in atributoXY:
+                if valorXY == "src" and atributoXY[valorXY][:7] == "http://":
+                    urlretrieve(atributoXY[valorXY], atributoXY[valorXY].split("/")[-1])
+                    
+                    atributoXY[valorXY] = atributoXY[valorXY].split("/")[-1]
+                    
+                    print("Descargando %s..." % atributoXY[valorXY])
+                    
+                
+    print(archv_smil)
         
 except IndexError:
     sys.exit("Usage:python3 karaoke.py file.smil.")
