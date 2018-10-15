0823dab23aa5f15cd806ea109352c57b7a68ced0
AlejandroEsLo <alex-lojim@hotmail.com>
2018-10-15 20:25:34 +0200

    Terminado karaoke.py y pasado pycodestyle(FALTA:mirar longitud linea 31 y pasar PEP8)

--- karaoke.py
+++ karaoke.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import sys
+from smallsmilhandler import SmallSMILHandler
 from xml.sax.handler import ContentHandler
 from xml.sax import make_parser
-import sys
 import json
-#from urllib.request import urlretrieve
-from smallsmilhandler import SmallSMILHandler
+from urllib.request import urlretrieve  # En python2 sin .request
+
 
 class KaraokeLocal():
 
@@ -16,44 +17,57 @@ class KaraokeLocal():
         parser.setContentHandler(my_class)
         parser.parse(open(fich_smil))
         self.my_archv = my_class.get_tags()
-                        
+
     def __str__(self):
         archv_smil = ""
         linea_final = ""
         for linea in self.my_archv:
             elementoX = linea[0]
             atributoXY = linea[1]
-            
-            #Iteramos por el archivo            
+
+            # Iteramos por el archivo
             for valorXY in atributoXY:
                 if atributoXY[valorXY] != "":
-                    elementoX += "\\t" + str(valorXY) + "=\"" + str(atributoXY[valorXY]) + "\""             
-                
-                linea_final = elementoX + "\\n"  
-                
+                    elementoX += "\\t" + str(valorXY) + "=\"" + str(atributoXY[valorXY]) + "\""
+
+                linea_final = elementoX + "\\n"
+
             archv_smil = archv_smil + linea_final + "\n"
-        
-        print(archv_smil)#Imprimimos archivo smil
-               
-    def to_json(self,fich_json):
-       #Fichero final Json            
+
+        print(archv_smil, end="")  # Imprimimos archivo smil
+        # Utilizamos el "end" para quitar el ultimo salto de linea
+
+    def to_json(self, fich_json):
+        # Fichero final Json
         json.dump(self.my_archv, open(fich_json, "w"))
-        
-   
-if __name__ == "__main__":   
-#Programa principal
+
+    def do_local(self):
+        # Descargamos archivos url http://
+        for valorXY in self.my_archv:
+            atributoXY = valorXY[1]
+            for valorXY in atributoXY:
+                if valorXY == "src" and atributoXY[valorXY][0:7] == "http://":
+                    urlretrieve(atributoXY[valorXY],
+                                atributoXY[valorXY].split("/")[-1])
+
+                    atributoXY[valorXY] = atributoXY[valorXY].split("/")[-1]
+                    # Prueba para ir viendo que va descargando...
+                    # print("Descargando %s..." % atributoXY[valorXY])
+
+
+if __name__ == "__main__":
+
+    # Programa principal
     try:
-        fich_smil = sys.argv[1]#Fichero principal
-        fich_json = sys.argv[1].replace(".smil",".json")#Fichero Json
+        fich_smil = sys.argv[1]  # Fichero principal
+        fich_json = sys.argv[1].replace(".smil", ".json")  # Fichero Json
         fich_karaoke = KaraokeLocal(fich_smil)
         fich_karaoke.__str__()
-        fich_karaoke.to_json(fich_json)#Pasamos fichero smil a fichero Json
+        fich_karaoke.to_json(fich_json)  # Pasamos fichero smil a fichero Json
+        fich_karaoke.do_local()  # Descargamos archivos
 
-        print("SALIDA(PRUEBA)")
-        
-        
     except IndexError:
         sys.exit("Usage:python3 karaoke.py file.smil.")
-        
+
     except FileNotFoundError:
         sys.exit("No existe el archivo introducido")
--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,45 +4,47 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
-    def __init__ (self):
+    def __init__(self):
 
-        #Creamos listas con sus correspondientes atributos
-        self.rootlayout = ["width","height","background-color"]
-        self.region = ["id","top","bottom","left","right"]
-        self.img = ["src","region","begin","dur"]
-        self.audio = ["src","begin","dur"]
-        self.textstream = ["src","region"]
-        
-        #Lista vacia para ir almacenando las listas anteriores al rellenarse
+        # Creamos listas con sus correspondientes atributos
+        self.rootlayout = ["width", "height", "background-color"]
+        self.region = ["id", "top", "bottom", "left", "right"]
+        self.img = ["src", "region", "begin", "dur"]
+        self.audio = ["src", "begin", "dur"]
+        self.textstream = ["src", "region"]
+
+        # Lista vacia para ir almacenando las listas anteriores al rellenarse
         self.data = []
-    
+
     def startElement(self, etiqueta, attrs):
 
-        #Creamos un diccionario con la etiqueta y sus atributos
+        # Creamos un diccionario con la etiqueta y sus atributos
         self.dicc = {"root-layout": self.rootlayout, "region": self.region,
                      "img": self.img, "audio": self.audio,
                      "textstream": self.textstream}
 
-        dicc_aux ={}
-        
+        dicc_aux = {}
+
         if etiqueta in self.dicc:
-            #Iteramos por todos los atributos de cada etiqueta
+            # Iteramos por todos los atributos de cada etiqueta
             for attrb in self.dicc[etiqueta]:
-                dicc_aux[attrb] = attrs.get(attrb, "")  
-        
-            self.data.append([etiqueta,dicc_aux]) # Añadir al final de la lista
-      
+                dicc_aux[attrb] = attrs.get(attrb, "")
+
+            # Añadir al final de la lista
+            self.data.append([etiqueta, dicc_aux])
+
     def get_tags(self):
-       
-       return self.data
+
+        return self.data
+
 
 if __name__ == "__main__":
 
-    parser = make_parser()#Creo parser
-    cHandler = SmallSMILHandler()#Creo manejador
-    parser.setContentHandler(cHandler)#Le paso el parser al manejador
+    parser = make_parser()  # Creo parser
+    cHandler = SmallSMILHandler()  # Creo manejador
+    parser.setContentHandler(cHandler)  # Le paso el parser al manejador
     parser.parse(open('karaoke.smil'))
     print(cHandler.get_tags())
-    
